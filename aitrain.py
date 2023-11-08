1820ec13ccddbbcbb67f2d7cc10d3cefc848f566
scooter7 <scooter.vineburgh@gmail.com>
2023-11-08 17:32:03 -0600

    Update aitrain.py

--- aitrain.py
+++ aitrain.py
@@ -19,14 +19,17 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
-    
+
     documents = SimpleDirectoryReader(directory_path).load_data()
-    processed_documents = [{'text': doc} for doc in documents]
-    
-    index = GPTSimpleVectorIndex(processed_documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
     index.directory_path = directory_path
+
     index.save_to_disk('index.json')
+
     return index
 
 # Chatbot function that uses the constructed index
