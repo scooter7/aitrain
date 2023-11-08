41c9efddaf35815dafe75308912d98ed6bfa51d0
scooter7 <scooter.vineburgh@gmail.com>
2023-11-08 13:50:06 -0600

    Update aitrain.py

--- aitrain.py
+++ aitrain.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import openai
+import base64
 from github import Github
 from datetime import datetime
-import base64
+import io
 import fitz
 
 # Initialize the GitHub and OpenAI API clients
@@ -14,21 +15,21 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title="AI-Powered LMS Chat")
 st.title('Welcome to the AI-Powered Learning Management System')
 
-# Define the function to fetch and decode the PDF from GitHub
+# Function to fetch and decode the PDF from GitHub
 def fetch_pdf_content():
     contents = repo.get_contents("docs/marketing_strategy_plan_methodology.pdf")
-    pdf_base64 = base64.b64decode(contents.content)
-    return pdf_base64
+    pdf_data = base64.b64decode(contents.content)
+    return pdf_data
 
-# Define the function to extract text from the PDF
+# Function to extract text from the PDF
 def extract_text_from_pdf(pdf_data):
-    with fitz.open(stream=pdf_data, filetype="pdf") as doc:
+    with fitz.open("pdf", pdf_data) as doc:
         text = ""
         for page in doc:
             text += page.get_text()
     return text
 
-# Define the function to interact with the OpenAI chat model
+# Function to interact with the OpenAI chat model
 def chat_with_gpt3(analysis_text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -41,21 +42,21 @@ pdf_data = fetch_pdf_content()
 intro_text = extract_text_from_pdf(pdf_data)
 st.write(intro_text)
 
-# Define the chat interface
+# Chat interface
 input_text = st.text_area("Enter your message:")
 send_button = st.button("Send")
 if send_button and input_text:
     response = chat_with_gpt3(input_text)
     st.write("Analysis: ", response)
 
-# Define the file uploader for users to upload completed materials
+# File uploader for users to upload completed materials
 uploaded_file = st.file_uploader("Upload your completed material here:", type=['pdf', 'docx', 'xlsx'])
 if uploaded_file is not None:
     file_contents = base64.b64encode(uploaded_file.getvalue()).decode()
     upload_path = f"content/completed_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{uploaded_file.name}"
     repo.create_file(upload_path, f"Upload completed material {uploaded_file.name}", file_contents)
 
-# Apply custom CSS to hide Streamlit's default UI elements
+# Custom CSS to hide Streamlit's default UI elements
 st.markdown("""
             <style>
             #MainMenu {visibility: hidden;}
