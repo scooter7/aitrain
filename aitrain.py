daf5a573c5f874b00968f3c81712b91a3ef7631e
scooter7 <scooter.vineburgh@gmail.com>
2023-11-08 17:51:17 -0600

    Update aitrain.py

--- aitrain.py
+++ aitrain.py
@@ -1,10 +1,10 @@
 import streamlit as st
+from github import Github
+import fitz  # PyMuPDF
 import os
 import sys
-from github import Github
 from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
-import logging
 
 # Check for OpenAI API key in Streamlit secrets
 if "OPENAI_API_KEY" not in st.secrets:
@@ -12,7 +12,6 @@ if "OPENAI_API_KEY" not in st.secrets:
     sys.exit(1)
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-logging.info(f"OPENAI_API_KEY: {openai_api_key}")
 
 # Check for GitHub token in Streamlit secrets
 if "GITHUB_TOKEN" not in st.secrets:
@@ -25,8 +24,22 @@ g = Github(github_token)
 # Initialize the GitHub repository
 repo = g.get_repo("scooter7/aitrain")
 
+# Function to extract text from PDF
+def extract_text_from_pdf(pdf_content):
+    with fitz.open(stream=pdf_content, filetype="pdf") as doc:
+        text = ""
+        for page in doc:
+            text += page.get_text()
+    return text
+
 # Function to construct the index
-def construct_index(directory_path):
+def construct_index():
+    # Get the content of the PDF file from the repo
+    pdf_content = repo.get_contents("docs/marketing_strategy_plan_methodology.pdf").decoded_content
+
+    # Extract text from the PDF
+    extracted_text = extract_text_from_pdf(pdf_content)
+
     max_input_size = 4096
     num_outputs = 512
     max_chunk_overlap = 20
@@ -34,15 +47,18 @@ def construct_index(directory_path):
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
-    documents = SimpleDirectoryReader(directory_path).load_data()
+    
+    # Create a document list with the content of the PDF
+    documents = [extracted_text]
+    
     index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index.save_to_disk('index.json')
     return index
 
 # Function to handle chat
-def chatbot(input_text, first_name, email):
+def chatbot(input_text):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    prompt = f"{first_name} ({email}): {input_text}"
+    prompt = input_text
     response = index.query(prompt, response_mode="compact")
     return response.response
 
@@ -50,21 +66,23 @@ def chatbot(input_text, first_name, email):
 def main():
     st.title("AI Marketing Strategy Assistant")
 
+    # Create the index on app startup
+    if 'index' not in st.session_state:
+        st.session_state.index = construct_index()
+
     # Create a container to hold the chat messages
     chat_container = st.container()
 
     # Create a form to enter a message and submit it
     with st.form(key="chat_form"):
-        first_name = st.text_input("Enter your first name:")
-        email = st.text_input("Enter your email address:")
-        input_text = st.text_area("Enter your message:")
-        submit_button = st.form_submit_button(label="Send")
+        input_text = st.text_area("Enter your question about the marketing strategy:")
+        submit_button = st.form_submit_button(label="Ask")
 
     if submit_button and input_text:
-        response = chatbot(input_text, first_name, email)
+        response = chatbot(input_text)
         with chat_container:
-            st.write(f"{first_name}: {input_text}")
-            st.write(f"Chatbot: {response}")
+            st.write(f"You: {input_text}")
+            st.write(f"AI: {response}")
 
 # Run the main function
 if __name__ == "__main__":
