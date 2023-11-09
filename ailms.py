d4bc59e743378a747367db4490a6e686e5e452bd
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 17:39:58 -0600

    Update ailms.py

--- ailms.py
+++ ailms.py
@@ -1,37 +1,51 @@
 import streamlit as st
+import fitz  # PyMuPDF
 import os
 from github import Github
 import openai
 from pypdf import PdfReader
 
-st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto")
+# Download necessary NLTK data
+# nltk.download('popular') # This line should be run separately if needed, not within the app
 
+# Set up the Streamlit page
+st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# Check for necessary secrets
 if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
     st.error("Please set the necessary secrets (OPENAI_API_KEY and GITHUB_TOKEN) on the Streamlit dashboard.")
     st.stop()
 
+# Set up API keys
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 github_token = st.secrets["GITHUB_TOKEN"]
 
+# Set up GitHub client
 g = Github(github_token)
 repo = g.get_repo("scooter7/aitrain")
 
+# Fetch the list of files in the docs directory
 contents = repo.get_contents("docs")
 document_titles = [content.name for content in contents if content.name.endswith(('.docx', '.xlsx'))]
 document_urls = {title: content.download_url for title, content in zip(document_titles, contents) if title.endswith(('.docx', '.xlsx'))}
 
+# Streamlit title and info
 st.title("Chat with Bain Report")
 st.info("This app allows you to ask questions about the Bain Report.", icon="📃")
 
+# Initialize session state for messages if not already present
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Function to load and index the Bain Report
 @st.experimental_singleton
 def load_data():
+    # Assuming the PDF file is in the same directory as the Streamlit app
     pdf_path = "docs/marketing_strategy_plan_methodology.pdf"
     if not os.path.exists(pdf_path):
         raise FileNotFoundError(f"The file {pdf_path} was not found.")
     
+    # Use PyPDF2 to extract text from the PDF
     reader = PdfReader(pdf_path)
     text = ""
     for page in reader.pages:
@@ -41,24 +55,29 @@ def load_data():
 
 report_text = load_data()
 
-if 'user_input' not in st.session_state:
-    st.session_state['user_input'] = ''
+# Chat input
+if prompt := st.text_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-user_input = st.text_input("Your question", key="user_input_key")
-if user_input:
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    st.session_state['user_input'] = ''
+    # Generate and display assistant response
+if prompt := st.text_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
+    # Generate and display assistant response
     with st.spinner("Thinking..."):
-        if any(title.lower() in user_input.lower() for title in document_titles):
-            matching_titles = [title for title in document_titles if title.lower() in user_input.lower()]
+        # Check if the user is asking for a document
+        if any(title.lower() in prompt.lower() for title in document_titles):
+            # Find the matching document title
+            matching_titles = [title for title in document_titles if title.lower() in prompt.lower()]
             if matching_titles:
-                matching_title = matching_titles[0]
+                # Provide the link to the matching document
+                matching_title = matching_titles[0]  # Assuming the first match is the desired one
                 document_url = document_urls[matching_title]
                 response_content = f"I found the document you're looking for: [{matching_title}]({document_url})"
             else:
                 response_content = "I couldn't find the document you're looking for."
         else:
+            # Handle other types of queries
             chat_messages = [{"role": "system", "content": "You are a helpful assistant. Answer the user's questions accurately."}]
             for message in st.session_state.messages:
                 chat_messages.append({"role": message["role"], "content": message["content"]})
@@ -70,7 +89,7 @@ if user_input:
             response_content = completion.choices[0].message['content']
         
         st.session_state.messages.append({"role": "assistant", "content": response_content})
-        st.session_state['user_input'] = ''
 
+# Display chat messages
 for message in st.session_state.messages:
     st.write(f"{message['role'].title()}: {message['content']}")
