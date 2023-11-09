87bb183684dd6f864d52f09ac7fa780d43b3a00f
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 17:01:10 -0600

    Update ailms.py

--- ailms.py
+++ ailms.py
@@ -1,11 +1,19 @@
 import streamlit as st
+import fitz  # PyMuPDF
+import os
 from github import Github
 import openai
 from pypdf import PdfReader
-import os
 
-# Set up the Streamlit page and API keys
+# Set up the Streamlit page
 st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# Check for necessary secrets
+if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
+    st.error("Please set the necessary secrets (OPENAI_API_KEY and GITHUB_TOKEN) on the Streamlit dashboard.")
+    sys.exit(1)
+
+# Set up API keys
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 github_token = st.secrets["GITHUB_TOKEN"]
 
@@ -15,49 +23,66 @@ repo = g.get_repo("scooter7/aitrain")
 
 # Function to extract text by stages from the PDF
 def extract_text_by_stages(pdf_path):
-    # Logic to extract text by stages from the PDF
-    # This would require parsing the PDF and identifying sections corresponding to each stage
-    stages_text = {
-        "Stage 1 - Values": "Text for Stage 1",
-        # ... other stages
-    }
+    doc = fitz.open(pdf_path)
+    stages_text = {}
+    current_stage = None
+    current_text = []
+
+    for page in doc:
+        text = page.get_text()
+        # Search for stage headings like "Stage 1 - Values"
+        if "Stage" in text and "-" in text:
+            if current_stage:
+                stages_text[current_stage] = "\n".join(current_text)
+            current_stage = text.strip().split("\n")[0]  # Assumes the stage title is at the start of a line
+            current_text = []
+        else:
+            current_text.append(text)
+    
+    if current_stage:  # Don't forget to save the last stage
+        stages_text[current_stage] = "\n".join(current_text)
+
     return stages_text
 
-# Function to handle file uploads and commit to GitHub
-def handle_file_upload(uploaded_file):
-    if uploaded_file is not None:
-        # Logic to save the file and commit to the "uploads" folder in the GitHub repo
-        pass
+# Function to save uploaded file
+def save_uploaded_file(uploaded_file):
+    with open(os.path.join("tempDir", uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return f.name
 
-# Load and index the Bain Report by stages
+# Function to upload file to GitHub
+def upload_to_github(file_path, repo, path_in_repo):
+    with open(file_path, "rb") as f:
+        content = f.read()
+    repo.create_file(path_in_repo, "commit message", content)
+
+# Display stages and handle file uploads
 stages_text = extract_text_by_stages("docs/marketing_strategy_plan_methodology.pdf")
 
-# Streamlit interface to present text and links for each stage
 for stage, text in stages_text.items():
     st.subheader(stage)
     st.write(text)
-    # Provide links to associated documents within the stage
-    # Logic to provide links to associated documents
+    # Add more logic here to provide links to associated documents
 
-# Streamlit interface for file upload
 uploaded_file = st.file_uploader("Upload your document", type=['docx', 'xlsx'])
-handle_file_upload(uploaded_file)
+if uploaded_file is not None:
+    file_path = save_uploaded_file(uploaded_file)
+    upload_to_github(file_path, repo, f"uploads/{uploaded_file.name}")
 
 # Chat interface
-# Initialize session state for messages if not already present
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Chat input
 if prompt := st.text_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display chat messages
 for message in st.session_state.messages:
     st.write(f"{message['role'].title()}: {message['content']}")
 
-# Generate and display assistant response
 if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
-    # Logic for handling user queries and uploaded documents
-    # This would involve using OpenAI to analyze the content of the uploaded documents and generate responses.
-    pass
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=st.session_state.messages[-1]["content"],
+        max_tokens=150
+    )
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].text})
