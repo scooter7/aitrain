3aff60c605171ecc94da1d661d64605d8d91363f
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 10:35:06 -0600

    Update aitrain2.py

--- aitrain2.py
+++ aitrain2.py
@@ -1,33 +1,41 @@
 import streamlit as st
+from github import Github
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from pypdf import PdfReader
 import nltk
-import logging
 import os
-import difflib
 
 # Download necessary NLTK data
 nltk.download('popular')
 
-# Set up the Streamlit app configuration
+# Set up the Streamlit page
 st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# Check for the OpenAI API key in the Streamlit secrets
-if "OPENAI_API_KEY" not in st.secrets:
-    st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
+# Check for necessary secrets
+if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
+    st.error("Please set the necessary secrets (OPENAI_API_KEY and GITHUB_TOKEN) on the Streamlit dashboard.")
     sys.exit(1)
 
-# Retrieve the OpenAI API key from the Streamlit secrets
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-logging.info(f"OPENAI_API_KEY: {openai_api_key}")
+# Set up API keys
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+github_token = st.secrets["GITHUB_TOKEN"]
 
-# Set up the title and information for the Streamlit app
+# Set up GitHub client
+g = Github(github_token)
+repo = g.get_repo("scooter7/aitrain")
+
+# Fetch the list of files in the docs directory
+contents = repo.get_contents("docs")
+document_titles = [content.name for content in contents if content.name.endswith('.pdf')]
+document_urls = {title: content.download_url for title, content in zip(document_titles, contents) if title.endswith('.pdf')}
+
+# Streamlit title and info
 st.title("Chat with Bain Report")
 st.info("This app allows you to ask questions about the Bain Report.", icon="📃")
 
-# Initialize the chat messages history if it doesn't exist
+# Initialize session state for messages if not already present
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about the Bain Report!"}
@@ -48,56 +56,46 @@ def load_data():
         for page in reader.pages:
             text += page.extract_text() + "\n"
         
-        # Create a document object with the extracted text
         docs = [Document(text=text, title="Bain Report")]
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
-# Load the indexed data
 index = load_data()
 
-# Initialize the chat engine if it doesn't exist
+# Initialize chat engine if not already present
 if "chat_engine" not in st.session_state:
     st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-# Function to determine if the user is requesting a document
-def is_requesting_document(user_query):
-    document_request_keywords = ['document', 'file', 'pdf', 'download', 'view']
-    return any(keyword in user_query.lower() for keyword in document_request_keywords)
-
-# Function to find the best matching document for a given query
-def find_matching_document(user_query, list_of_document_titles):
-    match = difflib.get_close_matches(user_query, list_of_document_titles, n=1, cutoff=0.6)
-    return match[0] if match else None
-
-# Function to handle the user query
-def handle_user_query(user_query):
-    if is_requesting_document(user_query):
-        document_title = find_matching_document(user_query, list_of_document_titles)
-        if document_title:
-            document_url = document_title_to_url_map[document_title]
-            return f"Here is the document you requested: [Link to {document_title}]({document_url})"
-        else:
-            return "I couldn't find the document you're looking for."
-    else:
-        response = st.session_state.chat_engine.chat(user_query)
-        return response.response
-
-# Prompt for user input and save to chat history
+# Chat input
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display the prior chat messages
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If the last message is not from the assistant, generate a new response
+# Generate and display assistant response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = handle_user_query(prompt)
-            st.write(response)
-            message = {"role": "assistant", "content": response}
+            # Check if the user is asking for a document
+            if any(title.lower() in prompt.lower() for title in document_titles):
+                # Find the matching document title
+                matching_titles = [title for title in document_titles if title.lower() in prompt.lower()]
+                if matching_titles:
+                    # Provide the link to the matching document
+                    matching_title = matching_titles[0]  # Assuming the first match is the desired one
+                    document_url = document_urls[matching_title]
+                    response_content = f"I found the document you're looking for: [{matching_title}]({document_url})"
+                else:
+                    response_content = "I couldn't find the document you're looking for."
+            else:
+                # Handle other types of queries
+                response = st.session_state.chat_engine.chat(prompt)
+                response_content = response.response
+            
+            st.write(response_content)
+            message = {"role": "assistant", "content": response_content}
             st.session_state.messages.append(message)
