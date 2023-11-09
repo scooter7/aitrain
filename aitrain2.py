c6fcf63dcf3cb92a42ef993359361685309af356
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 10:02:29 -0600

    Update aitrain2.py

--- aitrain2.py
+++ aitrain2.py
@@ -1,107 +1,103 @@
 import streamlit as st
-from github import Github
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from pypdf import PdfReader
 import nltk
+import logging
 import os
 import difflib
 
 # Download necessary NLTK data
 nltk.download('popular')
 
-# Set up Streamlit page configuration
+# Set up the Streamlit app configuration
 st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# Check if the necessary API keys are set in Streamlit secrets
-if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
-    st.error("Please set the necessary API keys in Streamlit secrets.")
+# Check for the OpenAI API key in the Streamlit secrets
+if "OPENAI_API_KEY" not in st.secrets:
+    st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
     sys.exit(1)
 
-# Set up API keys from Streamlit secrets
+# Retrieve the OpenAI API key from the Streamlit secrets
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-github_token = st.secrets["GITHUB_TOKEN"]
+logging.info(f"OPENAI_API_KEY: {openai_api_key}")
 
-# Initialize GitHub client
-g = Github(github_token)
-repo_name = "scooter7/aitrain"
-repo = g.get_repo(repo_name)
+# Set up the title and information for the Streamlit app
+st.title("Chat with Bain Report")
+st.info("This app allows you to ask questions about the Bain Report.", icon="📃")
 
-# Function to get the URL for a file in the GitHub repository
-def get_github_file_url(file_path):
-    return f"https://github.com/{repo_name}/blob/main/{file_path}"
-
-# Function to get a list of files in the docs folder
-def get_docs_files():
-    contents = repo.get_contents("docs")
-    return {content.name: content.path for content in contents if content.type == "file"}
-
-# Function to find the closest matching document name
-def find_closest_match(query, choices):
-    return difflib.get_close_matches(query, choices, n=1, cutoff=0.1)
-
-# Function to search for the most relevant document
-def search_docs(message, files_dict):
-    # Use difflib to find the closest match to the message
-    closest_matches = find_closest_match(message, files_dict.keys())
-    if closest_matches:
-        return files_dict[closest_matches[0]]
-    return None
-
-# Initialize chat messages if not already present in session state
+# Initialize the chat messages history if it doesn't exist
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about the Bain Report!"}
     ]
 
-# Load data function to index documents
-@st.cache(allow_output_mutation=True, show_spinner=False)
+# Function to load and index the Bain Report
+@st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Bain Report – hang tight!"):
+        # Assuming the PDF file is in the same directory as the Streamlit app
         pdf_path = "docs/marketing_strategy_plan_methodology.pdf"
         if not os.path.exists(pdf_path):
             raise FileNotFoundError(f"The file {pdf_path} was not found.")
         
+        # Use PyPDF2 to extract text from the PDF
         reader = PdfReader(pdf_path)
         text = ""
         for page in reader.pages:
             text += page.extract_text() + "\n"
         
+        # Create a document object with the extracted text
         docs = [Document(text=text, title="Bain Report")]
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
+# Load the indexed data
 index = load_data()
 
-# Initialize chat engine if not already present in session state
+# Initialize the chat engine if it doesn't exist
 if "chat_engine" not in st.session_state:
     st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-# Handle chat input
+# Function to determine if the user is requesting a document
+def is_requesting_document(user_query):
+    document_request_keywords = ['document', 'file', 'pdf', 'download', 'view']
+    return any(keyword in user_query.lower() for keyword in document_request_keywords)
+
+# Function to find the best matching document for a given query
+def find_matching_document(user_query, list_of_document_titles):
+    match = difflib.get_close_matches(user_query, list_of_document_titles, n=1, cutoff=0.6)
+    return match[0] if match else None
+
+# Function to handle the user query
+def handle_user_query(user_query):
+    if is_requesting_document(user_query):
+        document_title = find_matching_document(user_query, list_of_document_titles)
+        if document_title:
+            document_url = document_title_to_url_map[document_title]
+            return f"Here is the document you requested: [Link to {document_title}]({document_url})"
+        else:
+            return "I couldn't find the document you're looking for."
+    else:
+        response = st.session_state.chat_engine.chat(user_query)
+        return response.response
+
+# Prompt for user input and save to chat history
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display chat messages
+# Display the prior chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Generate and handle chat responses
+# If the last message is not from the assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            user_message = st.session_state.messages[-1]["content"]
-            docs_files = get_docs_files()
-            doc_path = search_docs(user_message, docs_files)
-            if doc_path:
-                file_url = get_github_file_url(doc_path)
-                response_message = f"Here is the document you requested: [link]({file_url})"
-            else:
-                response = st.session_state.chat_engine.chat(user_message)
-                response_message = response.response
-            
-            st.write(response_message)
-            message = {"role": "assistant", "content": response_message}
+            response = handle_user_query(prompt)
+            st.write(response)
+            message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message)
