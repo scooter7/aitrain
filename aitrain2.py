ff62d48fde95d7eb6e2a265a048f52738f5b23cf
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 11:58:09 -0600

    Update aitrain2.py

--- aitrain2.py
+++ aitrain2.py
@@ -1,46 +1,57 @@
 import streamlit as st
 from github import Github
+import difflib
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from pypdf import PdfReader
 import nltk
 import os
-import difflib
 
+# Download necessary NLTK data
 nltk.download('popular')
 
+# Set up the Streamlit page
 st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
+# Check for necessary secrets
 if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
     st.error("Please set the necessary secrets (OPENAI_API_KEY and GITHUB_TOKEN) on the Streamlit dashboard.")
     sys.exit(1)
 
+# Set up API keys
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 github_token = st.secrets["GITHUB_TOKEN"]
 
+# Set up GitHub client
 g = Github(github_token)
 repo = g.get_repo("scooter7/aitrain")
 
+# Fetch the list of files in the docs directory
 contents = repo.get_contents("docs")
-document_titles = [content.name for content in contents if content.name.endswith('.pdf')]
-document_urls = {title: content.download_url for title, content in zip(document_titles, contents) if title.endswith('.pdf')}
+document_titles = [content.name for content in contents if content.name.endswith(('.docx', '.xlsx'))]
+document_urls = {title: content.download_url for title, content in zip(document_titles, contents) if title.endswith(('.docx', '.xlsx'))}
 
+# Streamlit title and info
 st.title("Chat with Bain Report")
 st.info("This app allows you to ask questions about the Bain Report.", icon="📃")
 
+# Initialize session state for messages if not already present
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about the Bain Report!"}
     ]
 
+# Function to load and index the Bain Report
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Bain Report – hang tight!"):
+        # Assuming the PDF file is in the same directory as the Streamlit app
         pdf_path = "docs/marketing_strategy_plan_methodology.pdf"
         if not os.path.exists(pdf_path):
             raise FileNotFoundError(f"The file {pdf_path} was not found.")
         
+        # Use PyPDF2 to extract text from the PDF
         reader = PdfReader(pdf_path)
         text = ""
         for page in reader.pages:
@@ -53,27 +64,31 @@ def load_data():
 
 index = load_data()
 
+# Initialize chat engine if not already present
 if "chat_engine" not in st.session_state:
     st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
+# Chat input
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# Generate and display assistant response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            keywords = prompt.lower().split()
-            matching_title = difflib.get_close_matches(' '.join(keywords), document_titles, n=1)
-            if matching_title:
-                document_url = document_urls[matching_title[0]]
-                response_content = f"I found the document you're looking for: [{matching_title[0]}]({document_url})"
+            # Attempt to find a close match for the document title in the user's query
+            closest_matches = difflib.get_close_matches(prompt, document_titles, n=1, cutoff=0.1)
+            if closest_matches:
+                document_title = closest_matches[0]
+                document_url = document_urls[document_title]
+                response_content = f"I found the document you're looking for: [{document_title}]({document_url})"
             else:
-                response = st.session_state.chat_engine.chat(prompt)
-                response_content = response.response
+                response_content = "I couldn't find the document you're looking for. Please make sure to use the exact title of the document."
             
             st.write(response_content)
             message = {"role": "assistant", "content": response_content}
