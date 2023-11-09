92936df21a4d353bd13190b4c21c18c86cf38414
scooter7 <scooter.vineburgh@gmail.com>
2023-11-08 18:05:54 -0600

    Update aitrain.py

--- aitrain.py
+++ aitrain.py
@@ -1,97 +1,58 @@
 import streamlit as st
-from github import Github
-import PyPDF2
-import io
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain.chat_models import ChatOpenAI
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from PyPDF2 import PdfReader
+import nltk
 
-# Check for OpenAI API key in Streamlit secrets
-if "OPENAI_API_KEY" not in st.secrets:
-    st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
-    st.stop()
+# Download necessary NLTK data
+nltk.download('popular')
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets["openai_key"]
+st.title("Chat with Bain Report")
+st.info("This app allows you to ask questions about the Bain Report.", icon="📃")
 
-# Check for GitHub token in Streamlit secrets
-if "GITHUB_TOKEN" not in st.secrets:
-    st.error("Please set the GITHUB_TOKEN secret on the Streamlit dashboard.")
-    st.stop()
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about the Bain Report!"}
+    ]
 
-github_token = st.secrets["GITHUB_TOKEN"]
-g = Github(github_token)
-
-# Initialize the GitHub repository
-repo = g.get_repo("scooter7/aitrain")
-
-# Function to extract text from PDF
-def extract_text_from_pdf(pdf_content):
-    pdfReader = PyPDF2.PdfFileReader(io.BytesIO(pdf_content))
-    text = ""
-    for pageNum in range(pdfReader.numPages):
-        pageObj = pdfReader.getPage(pageNum)
-        text += pageObj.extractText()
-    return text
-
-# Function to construct the index
-def construct_index():
-    try:
-        # Get the content of the PDF file from the repo
-        contents = repo.get_contents("docs/marketing_strategy_plan_methodology.pdf")
-        if contents.encoding == "base64":
-            pdf_content = contents.decoded_content
-        else:
-            raise ValueError("Unsupported encoding for the PDF content")
-
-        # Extract text from the PDF
-        extracted_text = extract_text_from_pdf(pdf_content)
-
-        max_input_size = 4096
-        num_outputs = 512
-        max_chunk_overlap = 20
-        chunk_size_limit = 600
-
-        prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-        llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Bain Report – hang tight!"):
+        # Assuming the PDF file is in the same directory as the Streamlit app
+        pdf_path = "docs/marketing_strategy_plan_methodology.pdf"
+        if not os.path.exists(pdf_path):
+            raise FileNotFoundError(f"The file {pdf_path} was not found.")
         
-        # Create a document list with the content of the PDF
-        documents = [extracted_text]
+        # Use PyPDF2 to extract text from the PDF
+        reader = PdfReader(pdf_path)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
         
-        index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-        index.save_to_disk('index.json')
+        docs = [Document(text=text, title="Bain Report")]
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
-    except Exception as e:
-        st.error(f"An error occurred while constructing the index: {e}")
-        st.stop()
 
-# Function to handle chat
-def chatbot(input_text):
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    prompt = input_text
-    response = index.query(prompt, response_mode="compact")
-    return response.response
+index = load_data()
 
-# Main app logic
-def main():
-    st.title("AI Marketing Strategy Assistant")
+if "chat_engine" not in st.session_state:
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-    # Create the index on app startup
-    if 'index' not in st.session_state:
-        st.session_state.index = construct_index()
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Create a container to hold the chat messages
-    chat_container = st.container()
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-    # Create a form to enter a message and submit it
-    with st.form(key="chat_form"):
-        input_text = st.text_area("Enter your question about the marketing strategy:")
-        submit_button = st.form_submit_button(label="Ask")
-
-    if submit_button and input_text:
-        response = chatbot(input_text)
-        with chat_container:
-            st.write(f"You: {input_text}")
-            st.write(f"AI: {response}")
-
-# Run the main function
-if __name__ == "__main__":
-    main()
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
