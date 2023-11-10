94a5a4744f3ed67997d73ab1015fb6743bf9b40d
scooter7 <scooter.vineburgh@gmail.com>
2023-11-09 19:49:24 -0600

    Update ailms.py

--- ailms.py
+++ ailms.py
@@ -3,7 +3,6 @@ import fitz  # PyMuPDF
 import os
 from github import Github
 import openai
-from pypdf import PdfReader
 import difflib
 
 # Set up the Streamlit page
@@ -31,30 +30,35 @@ def extract_text_by_stages(pdf_path):
 
     for page in doc:
         text = page.get_text()
+        # Search for stage headings like "Stage 1 - Values"
         if "Stage" in text and "-" in text:
             if current_stage:
                 stages_text[current_stage] = "\n".join(current_text)
-            current_stage = text.strip().split("\n")[0]
+            current_stage = text.strip().split("\n")[0]  # Assumes the stage title is at the start of a line
             current_text = []
         else:
             current_text.append(text)
     
-    if current_stage:
+    if current_stage:  # Don't forget to save the last stage
         stages_text[current_stage] = "\n".join(current_text)
 
     return stages_text
 
-# Function to save uploaded file
-def save_uploaded_file(uploaded_file):
-    with open(os.path.join("tempDir", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return f.name
+# Function to get document titles and URLs from the docs directory in the repository
+def get_document_titles_and_urls(repo):
+    contents = repo.get_contents("docs")  # Gets the contents of the 'docs' directory
+    document_titles = []
+    document_urls = {}
 
-# Function to upload file to GitHub
-def upload_to_github(file_path, repo, path_in_repo):
-    with open(file_path, "rb") as f:
-        content = f.read()
-    repo.create_file(path_in_repo, "commit message", content)
+    for content_file in contents:
+        if content_file.type == "file" and content_file.name.endswith(('.pdf', '.docx', '.xlsx')):
+            document_titles.append(content_file.name)
+            document_urls[content_file.name] = content_file.download_url
+
+    return document_titles, document_urls
+
+# Fetch the actual document titles and URLs
+document_titles, document_urls = get_document_titles_and_urls(repo)
 
 # Display stages and handle file uploads
 stages_text = extract_text_by_stages("docs/marketing_strategy_plan_methodology.pdf")
@@ -63,11 +67,6 @@ for stage, text in stages_text.items():
     st.subheader(stage)
     st.write(text)
 
-uploaded_file = st.file_uploader("Upload your document", type=['docx', 'xlsx'])
-if uploaded_file is not None:
-    file_path = save_uploaded_file(uploaded_file)
-    upload_to_github(file_path, repo, f"uploads/{uploaded_file.name}")
-
 # Chat interface
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -75,9 +74,11 @@ if "messages" not in st.session_state:
 if prompt := st.text_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+for message in st.session_state.messages:
+    st.write(f"{message['role'].title()}: {message['content']}")
+
+if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
     document_keywords = ['document', 'file', 'download', 'link', 'template', 'worksheet', 'form']
-    document_titles = ["Sample Document Title"]  # Replace with actual document titles
-    document_urls = {"Sample Document Title": "http://example.com/document"}  # Replace with actual URLs
 
     if any(keyword in prompt.lower() for keyword in document_keywords):
         closest_matches = difflib.get_close_matches(prompt.lower(), [title.lower() for title in document_titles], n=5, cutoff=0.3)
@@ -95,9 +96,6 @@ if prompt := st.text_input("Your question"):
             model="gpt-4",
             messages=formatted_messages
         )
-        response_content = completion.choices[0].message['content']
+        response_content = completion.choices[0].message.content
 
     st.session_state.messages.append({"role": "assistant", "content": response_content})
-
-for message in st.session_state.messages:
-    st.write(f"{message['role'].title()}: {message['content']}")
