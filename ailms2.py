6e60e83e1421a80e55596beebca3ca14d76d1d41
scooter7 <scooter.vineburgh@gmail.com>
2023-11-10 17:01:35 -0600

    Update ailms2.py

--- ailms2.py
+++ ailms2.py
@@ -8,18 +8,23 @@ import openpyxl
 from docx import Document
 import fitz
 
+# Set Streamlit page configuration
 st.set_page_config(page_title="Chat with the Bain Report", page_icon="🦙", layout="centered", initial_sidebar_state="auto")
 
+# Check for necessary secrets
 if "OPENAI_API_KEY" not in st.secrets or "GITHUB_TOKEN" not in st.secrets:
     st.error("Please set the necessary secrets (OPENAI_API_KEY and GITHUB_TOKEN) on the Streamlit dashboard.")
     sys.exit(1)
 
+# Set API keys
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 github_token = st.secrets["GITHUB_TOKEN"]
 
+# Initialize GitHub
 g = Github(github_token)
 repo = g.get_repo("scooter7/aitrain")
 
+# Function to upload file to GitHub
 def upload_to_github(file_path, repo, path_in_repo):
     with open(file_path, "rb") as file:
         content = file.read()
@@ -32,12 +37,14 @@ def upload_to_github(file_path, repo, path_in_repo):
         repo.create_file(git_file, "Creating new file", content)
         st.success('File created on GitHub')
 
+# Function to save uploaded file
 def save_uploaded_file(uploaded_file):
     file_path = os.path.join("uploads", uploaded_file.name)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     return file_path
 
+# Function to get document titles and GitHub URLs
 def get_document_titles_and_github_urls(repo):
     contents = repo.get_contents("docs")
     document_titles = []
@@ -45,16 +52,11 @@ def get_document_titles_and_github_urls(repo):
     for content_file in contents:
         if content_file.type == "file" and content_file.name.endswith(('.pdf', '.docx', '.xlsx', '.pptx')):
             document_titles.append(content_file.name)
-            document_urls[content_file.name] = f"https://github.com/{repo.full_name}/blob/main/docs/{content_file.name}"
+            document_url = f"https://github.com/{repo.full_name}/blob/main/docs/{content_file.name}"
+            document_urls[content_file.name] = document_url
     return document_titles, document_urls
 
-document_titles, document_urls = get_document_titles_and_github_urls(repo)
-
-action_items = find_action_items_in_stage(current_stage_content)
-map_action_items_to_files(action_items, document_titles, document_urls)
-for title, url in action_items.items():
-    st.markdown(f"- [{title}]({url})")
-
+# Function to extract text by stages from a PowerPoint presentation
 def extract_text_by_stages(pptx_path):
     prs = Presentation(pptx_path)
     stages_text = {
@@ -76,6 +78,7 @@ def extract_text_by_stages(pptx_path):
         stages_content[stage] = "\n".join(text_content)
     return stages_content
 
+# Function to find action items in a stage
 def find_action_items_in_stage(stage_content):
     action_items = {}
     lines = stage_content.split("\n")
@@ -85,16 +88,19 @@ def find_action_items_in_stage(stage_content):
             action_items[action_item_title] = None
     return action_items
 
+# Function to map action items to files
 def map_action_items_to_files(action_items, document_titles, document_urls):
     for item in action_items.keys():
         closest_match = difflib.get_close_matches(item, document_titles, n=1, cutoff=0.5)
         if closest_match:
             action_items[item] = document_urls[closest_match[0]]
 
+# Function to extract text from a DOCX file
 def extract_text_from_docx(docx_path):
     doc = Document(docx_path)
     return "\n".join([paragraph.text for paragraph in doc.paragraphs])
 
+# Function to extract data from an XLSX file
 def extract_data_from_xlsx(xlsx_path):
     workbook = openpyxl.load_workbook(xlsx_path)
     text = ""
@@ -104,13 +110,14 @@ def extract_data_from_xlsx(xlsx_path):
             text += " ".join([str(cell) if cell is not None else "" for cell in row]) + "\n"
     return text
 
+# Function to summarize text
 def summarize_text(text, max_length=500):
     if len(text) > max_length:
         return text[:max_length] + "..."
     return text
 
-document_titles, document_urls = get_document_titles_and_urls(repo)
-
+# Main code
+document_titles, document_urls = get_document_titles_and_github_urls(repo)
 stages_content = extract_text_by_stages("docs/marketing_strategy_plan_methodology.pptx")
 
 if 'current_stage_index' not in st.session_state:
@@ -140,8 +147,6 @@ if uploaded_file is not None:
     file_content = ""
     if uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         file_content = extract_text_from_docx(file_path)
-    elif uploaded_file.type == "application/pdf":
-        file_content = extract_text_from_pdf(file_path)
     elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         file_content = extract_data_from_xlsx(file_path)
     summarized_content = summarize_text(file_content)
